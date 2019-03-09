07711eab511bb4e67c959f2b951b7a770af8a7b6
Henry Panora <hpanora@gmail.com>
2019-03-09 03:05:03 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,19 +6,16 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-      {:name: "Bavolio", :age: "17", attitude: "worried"
-        },
+      {name: "Bavolio", age: "17", attitude: "worried"},
       {name: "Mercutio", age: "18", attitude: "hot-headed"}
-  ]
-}
+  ]}
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-      {name: "Steven", age: "30", attitude: "confused"
-        },
+      {name: "Steven", age: "30", attitude: "confused"},
       {name: "Nurse", age: "44", attitude: "worried"}
   ]
  }
